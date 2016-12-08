98328546d932a67171c21c2aae70d072813ab388
rcicero <rafacicero.silva@gmail.com>
2016-12-08 16:10:23 -0200

    first commit

---
+++ src/main/java/com/example/PocDeployBeanstalkApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PocDeployBeanstalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PocDeployBeanstalkApplication.class, args);
+	}
+}
